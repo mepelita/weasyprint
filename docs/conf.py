82519941134012b412e0c290c44c4079499d2638
aproinov <98790856+aproinov@users.noreply.github.com>
2023-08-20 06:23:24 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Weasyprint Project"
+copyright = "2023, RTFD core team"
+author = "RTFD core team"
 
 
 # -- General configuration ---------------------------------------------------
